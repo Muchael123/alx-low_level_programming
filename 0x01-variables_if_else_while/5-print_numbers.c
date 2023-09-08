534e2d5f3e920d07fbc9ffa35e738aaf5eeb0883
Muchael123 <Mainamichael2849@gmail.com>
2023-09-08 15:33:55 +0300

    hello

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  if else statements
+ *  Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+return (0);
+}
