5722d2b8ede2e2128e210c037426b9869b67b047
Muchael123 <Mainamichael2849@gmail.com>
2023-09-08 16:32:07 +0300

    hello c letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,7 @@ int main(void)
 char letter = 'a';
 while (letter <= 'z')
 {
-if (letter == 'q' || letter == 'e')
-{
-continue;
-}
-else
+if (letter != 'q' && letter != 'e')
 {
 putchar(letter);
 }
