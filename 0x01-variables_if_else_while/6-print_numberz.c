f0afd07459b3d8ab2981e0d12746cf4535375c2d
Muchael123 <Mainamichael2849@gmail.com>
2023-09-08 15:50:05 +0300

    hello c nums

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  if else statements
+ *  Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+char letter = '0';
+while (letter <= '9')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
