3f269939ad082b195a1ef901a48013929d9bb2d3
Muchael123 <Mainamichael2849@gmail.com>
2023-09-18 11:18:51 +0300

    I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 /**
 * _puts - prints a string
 * @str: the string
-*
+*author: Maich
 * Return: the length of the string
 */
 void _puts(char *str)
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev -> printing a string in reverse
+ * @s: the string to be printed in rev
+ */
+
+void print_rev(char *s)
+{
+int i, j;
+j = 0;
+for (; s[j] != 0;)
+j++;
+for (i = j - 1; i >= 0; i--)
+_putchar(s[i]);
+}
